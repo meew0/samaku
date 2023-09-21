cd74229eb4baed72efabd2b4e96e843c56fd5d0a
meew0 <blactbt@live.de>
2023-09-21 22:45:16 +0200

    Statically link to bestsource, attempt 1

--- bestsource-sys/build.rs
+++ bestsource-sys/build.rs
@@ -6,15 +6,15 @@ use std::path::{Path, PathBuf};
 fn main() {
     let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     let bestsource_library_dir = Path::new(&manifest_dir).join("build");
-    if !bestsource_library_dir.join("libbestsourcew.so").exists() {
-        panic!("Missing compiled libbestsourcew.so! Please build BestSource according to the instructions in the README.");
+    if !bestsource_library_dir.join("libbestsourcew.a").exists() {
+        panic!("Missing compiled libbestsourcew.a! Please build BestSource according to the instructions in the README.");
     }
 
-    println!("cargo:rustc-link-lib=bestsourcew");
     println!(
         "cargo:rustc-link-search=native={}",
         bestsource_library_dir.display()
     );
+    println!("cargo:rustc-link-lib=static:+whole-archive,+bundle=bestsourcew");
     println!("cargo:rerun-if-changed=meson.build");
     println!("cargo:rerun-if-changed=wrapper/wrapper.cpp");
     println!("cargo:rerun-if-changed=wrapper/wrapper.h");
--- bestsource-sys/src/lib.rs
+++ bestsource-sys/src/lib.rs
@@ -1,5 +1,6 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
+#![feature(packed_bundled_libs)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
