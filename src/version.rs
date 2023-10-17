526ace61375f210dc038e1c95e99f1d008e269a2
meew0 <blactbt@live.de>
2023-10-17 18:20:10 +0200

    Use 9 hex digits from the git hash for the version, as Aegisub does

--- src/version.rs
+++ src/version.rs
@@ -16,6 +16,6 @@ pub struct Long;
 
 impl std::fmt::Display for Long {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}-{}", CARGO_VERSION, &GIT_HASH[0..8])
+        write!(f, "{}-{}", CARGO_VERSION, &GIT_HASH[0..9])
     }
 }
