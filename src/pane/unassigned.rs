578be3b53c6c54f6b7622b1e03ed912cb13713e0
meew0 <meew0@mz.sb>
2025-09-16 20:18:54 +0200

    Unassigned pane tweaks
    - Alphabetically sort the assignment buttons
    - Make the button row wrap

--- src/pane/unassigned.rs
+++ src/pane/unassigned.rs
@@ -7,13 +7,16 @@ pub struct State;
 impl super::LocalState for State {
     fn view(&self, self_pane: super::Pane, _global_state: &crate::Samaku) -> super::View<'_> {
         // Collect registered panes across the entire codebase
-        let pane_type_row = iced::widget::Row::with_children(
-            inventory::iter::<super::Shell>.into_iter().map(|shell| {
-                iced::widget::button(shell.name)
-                    .on_press(message::Message::SetPaneType(self_pane, shell.constructor))
-                    .into()
-            }),
-        );
+        let mut shells: Vec<&'static super::Shell> =
+            inventory::iter::<super::Shell>.into_iter().collect();
+        shells.sort_by_key(|shell| shell.name);
+        let pane_type_row = iced::widget::Row::with_children(shells.into_iter().map(|shell| {
+            iced::widget::button(shell.name)
+                .on_press(message::Message::SetPaneType(self_pane, shell.constructor))
+                .into()
+        }))
+        .spacing(10)
+        .wrap();
 
         super::View {
         title: iced::widget::text("Unassigned pane").into(),
@@ -22,7 +25,7 @@ impl super::LocalState for State {
             iced::widget::column![
                     iced::widget::text("Unassigned pane").size(20),
                     "Press F2 to split vertically, F3 to split horizontally, or click one of the buttons below to set the pane's type.",
-                    pane_type_row.spacing(10),
+                    pane_type_row,
                 ]
                 .spacing(20)
         )
