4a811d62b79552061dc2d23a146b52174c351c67
meew0 <meew0@mz.sb>
2025-09-16 20:35:33 +0200

    Format fix

--- src/pane/unassigned.rs
+++ src/pane/unassigned.rs
@@ -19,19 +19,18 @@ impl super::LocalState for State {
         .wrap();
 
         super::View {
-        title: iced::widget::text("Unassigned pane").into(),
-        content:
-        iced::widget::container(
-            iced::widget::column![
-                    iced::widget::text("Unassigned pane").size(20),
-                    "Press F2 to split vertically, F3 to split horizontally, or click one of the buttons below to set the pane's type.",
-                    pane_type_row,
-                ]
-                .spacing(20)
-        )
+            title: iced::widget::text("Unassigned pane").into(),
+            content: iced::widget::container(
+                iced::widget::column![
+                            iced::widget::text("Unassigned pane").size(20),
+                            "Press F2 to split vertically, F3 to split horizontally, or click one of the buttons below to set the pane's type.",
+                            pane_type_row,
+                        ]
+                    .spacing(20)
+            )
             .center_x(iced::Length::Fill)
             .center_y(iced::Length::Fill)
             .into(),
-    }
+        }
     }
 }
