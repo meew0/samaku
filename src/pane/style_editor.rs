12866343c0ff5d0377a432a3b74747058ebfcd58
meew0 <meew0@mz.sb>
2026-04-03 15:32:01 +0200

    Split the style editor `view` method

--- src/pane/style_editor.rs
+++ src/pane/style_editor.rs
@@ -16,131 +16,15 @@ impl super::LocalState for State {
         self_pane: super::Pane,
         global_state: &'a crate::Samaku,
     ) -> super::View<'a> {
-        let styles = global_state.subtitles.styles.as_slice();
-
-        // We know that this conversion is sound, since we statically assert that `StyleWrapper` and
-        // `Style` have the same size and alignment. However, Rust does not allow us to do this safely,
-        // so we have to use unsafe.
-        let wrapped_styles: &[StyleWrapper] =
-            unsafe { std::slice::from_raw_parts(styles.as_ptr().cast(), styles.len()) };
-
-        let selection_list = iced_aw::widgets::selection_list_with(
-            wrapped_styles,
-            move |selection_index, _| {
-                message::Message::Pane(
-                    self_pane,
-                    message::Pane::StyleEditorStyleSelected(selection_index),
-                )
-            },
-            14.0,
-            2.0,
-            iced_aw::style::selection_list::primary,
-            Some(self.selected_style_index),
-            crate::DEFAULT_FONT,
-        )
-        .width(iced::Length::Fixed(200.0))
-        .height(iced::Length::Fixed(200.0));
-
-        let create_button = iced::widget::button(iced::widget::text("Create new"))
-            .on_press(message::Message::CreateStyle);
-        let delete_button = iced::widget::button(iced::widget::text("Delete")).on_press_maybe(
-            // Do not allow deleting the first style
-            (self.selected_style_index != 0)
-                .then_some(message::Message::DeleteStyle(self.selected_style_index)),
-        );
-
-        let left_column = iced::widget::column![
-            iced::widget::text("Styles").size(20),
-            selection_list,
-            iced::widget::row![create_button, delete_button].spacing(5)
-        ]
-        .spacing(5);
-
-        // Right column starts here
-
-        let i = self.selected_style_index;
-        let selected_style = &global_state.subtitles.styles[i];
-
-        let bold_checkbox = iced::widget::checkbox(selected_style.bold)
-            .label("Bold")
-            .on_toggle(move |val| message::Message::SetStyleBold(i, val));
-        let italic_checkbox = iced::widget::checkbox(selected_style.italic)
-            .label("Italic")
-            .on_toggle(move |val| message::Message::SetStyleItalic(i, val));
-        let underline_checkbox = iced::widget::checkbox(selected_style.underline)
-            .label("Underline")
-            .on_toggle(move |val| message::Message::SetStyleUnderline(i, val));
-        let strike_out_checkbox = iced::widget::checkbox(selected_style.strike_out)
-            .label("Strike out")
-            .on_toggle(move |val| message::Message::SetStyleStrikeOut(i, val));
-
-        let flags_row = iced::widget::row![
-            bold_checkbox,
-            italic_checkbox,
-            underline_checkbox,
-            strike_out_checkbox
-        ];
-
-        let right_column = iced::widget::column![flags_row].spacing(5);
-
-        // Preview starts here
-
-        // We don't need to actually compile the subtitles here, since the `preview_events` will never use any NDE filters. So we can
-        // just pretend they have already been compiled.
-        let ass = media::subtitle::OpaqueTrack::from_compiled(
+        let left_column = left_column(self_pane, global_state, self.selected_style_index);
+        let right_column = right_column(global_state, self.selected_style_index);
+        let preview = preview(
+            global_state,
+            self.selected_style_index,
             self.preview_events.iter_events(),
-            std::slice::from_ref(selected_style),
-            // Match the script metadata we use globally, except, ignore any extra info (since it never really contains any useful data
-            // anyway, and would be costly to clone) and set the playback resolution to a useful value to maximise visibility.
-            // TODO: make the playback resolution scale configurable
-            &subtitle::ScriptInfo {
-                wrap_style: global_state.subtitles.script_info.wrap_style,
-                scaled_border_and_shadow: global_state
-                    .subtitles
-                    .script_info
-                    .scaled_border_and_shadow,
-                kerning: global_state.subtitles.script_info.kerning,
-                timer: global_state.subtitles.script_info.timer,
-                ycbcr_matrix: global_state.subtitles.script_info.ycbcr_matrix,
-                playback_resolution: subtitle::Resolution {
-                    x: i32::from(PREVIEW_WIDTH) * 2,
-                    y: i32::from(PREVIEW_HEIGHT) * 2,
-                },
-                extra_info: HashMap::new(),
-            },
         );
 
-        // Render the preview subtitles
-        let images = {
-            let mut view_state = global_state.view.borrow_mut();
-
-            let frame_size = subtitle::Resolution {
-                x: i32::from(PREVIEW_WIDTH),
-                y: i32::from(PREVIEW_HEIGHT),
-            };
-
-            view_state.subtitle_renderer.render_subtitles_onto_base(
-                &ass,
-                TRANSPARENT_IMAGE.clone(),
-                model::FrameNumber(0_i32),
-                media::FrameRate {
-                    numerator: 24,
-                    denominator: 1,
-                },
-                frame_size,
-                frame_size,
-            )
-        };
-
-        // Create an `ImageStack` showing the preview subs
-        let image_stack = view::widget::ImageStack::new(images, view::widget::EmptyProgram)
-            .set_image_size_override(iced::Size {
-                width: u32::from(PREVIEW_WIDTH),
-                height: u32::from(PREVIEW_HEIGHT),
-            });
-
-        let content =
-            iced::widget::column![image_stack, iced::widget::row![left_column, right_column]];
+        let content = iced::widget::column![preview, iced::widget::row![left_column, right_column]];
 
         super::View {
             title: iced::widget::text("Style editor").into(),
@@ -182,6 +66,144 @@ inventory::submit! {
         "Style editor",
         || Box::new(State::default())
     )
+}
+
+fn left_column(
+    self_pane: super::Pane,
+    global_state: &'_ crate::Samaku,
+    selected_style_index: usize,
+) -> iced::widget::Column<'_, message::Message> {
+    let styles = global_state.subtitles.styles.as_slice();
+
+    // We know that this conversion is sound, since we statically assert that `StyleWrapper` and
+    // `Style` have the same size and alignment. However, Rust does not allow us to do this safely,
+    // so we have to use unsafe.
+    let wrapped_styles: &[StyleWrapper] =
+        unsafe { std::slice::from_raw_parts(styles.as_ptr().cast(), styles.len()) };
+
+    let selection_list = iced_aw::widgets::selection_list_with(
+        wrapped_styles,
+        move |selection_index, _| {
+            message::Message::Pane(
+                self_pane,
+                message::Pane::StyleEditorStyleSelected(selection_index),
+            )
+        },
+        14.0,
+        2.0,
+        iced_aw::style::selection_list::primary,
+        Some(selected_style_index),
+        crate::DEFAULT_FONT,
+    )
+    .width(iced::Length::Fixed(200.0))
+    .height(iced::Length::Fixed(200.0));
+
+    let create_button = iced::widget::button(iced::widget::text("Create new"))
+        .on_press(message::Message::CreateStyle);
+    let delete_button = iced::widget::button(iced::widget::text("Delete")).on_press_maybe(
+        // Do not allow deleting the first style
+        (selected_style_index != 0).then_some(message::Message::DeleteStyle(selected_style_index)),
+    );
+
+    iced::widget::column![
+        iced::widget::text("Styles").size(20),
+        selection_list,
+        iced::widget::row![create_button, delete_button].spacing(5)
+    ]
+    .spacing(5)
+}
+
+fn right_column(
+    global_state: &'_ crate::Samaku,
+    selected_style_index: usize,
+) -> iced::widget::Column<'_, message::Message> {
+    let i = selected_style_index;
+    let selected_style = &global_state.subtitles.styles[i];
+
+    let bold_checkbox = iced::widget::checkbox(selected_style.bold)
+        .label("Bold")
+        .on_toggle(move |val| message::Message::SetStyleBold(i, val));
+    let italic_checkbox = iced::widget::checkbox(selected_style.italic)
+        .label("Italic")
+        .on_toggle(move |val| message::Message::SetStyleItalic(i, val));
+    let underline_checkbox = iced::widget::checkbox(selected_style.underline)
+        .label("Underline")
+        .on_toggle(move |val| message::Message::SetStyleUnderline(i, val));
+    let strike_out_checkbox = iced::widget::checkbox(selected_style.strike_out)
+        .label("Strike out")
+        .on_toggle(move |val| message::Message::SetStyleStrikeOut(i, val));
+
+    let flags_row = iced::widget::row![
+        bold_checkbox,
+        italic_checkbox,
+        underline_checkbox,
+        strike_out_checkbox
+    ];
+
+    iced::widget::column![flags_row].spacing(5)
+}
+
+fn preview<'a, 'b, 'c>(
+    global_state: &'a crate::Samaku,
+    selected_style_index: usize,
+    preview_events: impl Iterator<Item = &'b subtitle::Event<'c>> + 'b,
+) -> iced::Element<'a, message::Message>
+where
+    'c: 'b,
+{
+    let selected_style = &global_state.subtitles.styles[selected_style_index];
+
+    // We don't need to actually compile the subtitles here, since the `preview_events` will never use any NDE filters. So we can
+    // just pretend they have already been compiled.
+    let ass = media::subtitle::OpaqueTrack::from_compiled(
+        preview_events,
+        std::slice::from_ref(selected_style),
+        // Match the script metadata we use globally, except, ignore any extra info (since it never really contains any useful data
+        // anyway, and would be costly to clone) and set the playback resolution to a useful value to maximise visibility.
+        // TODO: make the playback resolution scale configurable
+        &subtitle::ScriptInfo {
+            wrap_style: global_state.subtitles.script_info.wrap_style,
+            scaled_border_and_shadow: global_state.subtitles.script_info.scaled_border_and_shadow,
+            kerning: global_state.subtitles.script_info.kerning,
+            timer: global_state.subtitles.script_info.timer,
+            ycbcr_matrix: global_state.subtitles.script_info.ycbcr_matrix,
+            playback_resolution: subtitle::Resolution {
+                x: i32::from(PREVIEW_WIDTH) * 2,
+                y: i32::from(PREVIEW_HEIGHT) * 2,
+            },
+            extra_info: HashMap::new(),
+        },
+    );
+
+    // Render the preview subtitles
+    let images = {
+        let mut view_state = global_state.view.borrow_mut();
+
+        let frame_size = subtitle::Resolution {
+            x: i32::from(PREVIEW_WIDTH),
+            y: i32::from(PREVIEW_HEIGHT),
+        };
+
+        view_state.subtitle_renderer.render_subtitles_onto_base(
+            &ass,
+            TRANSPARENT_IMAGE.clone(),
+            model::FrameNumber(0_i32),
+            media::FrameRate {
+                numerator: 24,
+                denominator: 1,
+            },
+            frame_size,
+            frame_size,
+        )
+    };
+
+    // Create an `ImageStack` showing the preview subs
+    view::widget::ImageStack::new(images, view::widget::EmptyProgram)
+        .set_image_size_override(iced::Size {
+            width: u32::from(PREVIEW_WIDTH),
+            height: u32::from(PREVIEW_HEIGHT),
+        })
+        .into()
 }
 
 fn default_preview_events() -> subtitle::EventTrack {
